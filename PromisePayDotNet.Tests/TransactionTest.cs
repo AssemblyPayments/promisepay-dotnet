c3a694c9c242098b45383441c1a26f4e3a27a639
Igor Sidorov <idsidorov@gmail.com>
2015-08-11 23:13:43 +0300

    transactions tests

--- PromisePayDotNet.Tests/TransactionTest.cs
+++ PromisePayDotNet.Tests/TransactionTest.cs
@@ -1,15 +1,16 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.IO;
 using Newtonsoft.Json;
+using NUnit.Framework;
 using PromisePayDotNet.DTO;
 using PromisePayDotNet.Implementations;
 using System;
+using RestSharp;
 
 namespace PromisePayDotNet.Tests
 {
-    [TestClass]
-    public class TransactionTest
+    public class TransactionTest : AbstractTest
     {
-        [TestMethod]
+        [Test]
         public void TransactionDeserialization()
         {
             var jsonStr = "{\"id\": \"8d8237c2-8598-4100-9fa5-f4ced75e7d76\",\"created_at\": \"2014-12-29T09:40:47.046Z\",\"updated_at\": \"2014-12-29T09:40:47.489Z\",\"description\": \"Buyer Fee @ 10%\",\"amount\": 5000,\"currency\":\"USD\",\"type\":\"debit\",\"from\": \"Escrow Vault\",\"to\": \"Awesome Websites\",\"related\": {\"transactions\":\"6a5525cf-e82f-40e7-995a-ad747185052a\"},\"links\":{\"self\":\"/transactions/8d8237c2-8598-4100-9fa5-f4ced75e7d76\",\"users\":\"/transactions/8d8237c2-8598-4100-9fa5-f4ced75e7d76/users\",\"fees\":\"/transactions/8d8237c2-8598-4100-9fa5-f4ced75e7d76/fees\"}}";
@@ -17,30 +18,33 @@ namespace PromisePayDotNet.Tests
             Assert.AreEqual("8d8237c2-8598-4100-9fa5-f4ced75e7d76", transaction.Id);
         }
 
-        [TestMethod]
+        [Test]
         public void ListTransactionsSuccessful()
         {
             //First, create a user, so we'll have at least one 
-            var repo = new TransactionRepository();
+            var content = File.ReadAllText("../../Fixtures/transactions_list.json");
+            var client = GetMockClient(content);
+            var repo = new TransactionRepository(client.Object);
             //Then, list users
             var transactions = repo.ListTransactions(200);
-
             Assert.IsNotNull(transactions);
         }
 
-        [TestMethod]
+        [Test]
         [ExpectedException(typeof(ArgumentException))]
         public void ListTransactionsNegativeParams()
         {
-            var repo = new TransactionRepository();
+            var client = GetMockClient("");
+            var repo = new TransactionRepository(client.Object);
             repo.ListTransactions(-10, -20);
         }
 
-        [TestMethod]
+        [Test]
         [ExpectedException(typeof(ArgumentException))]
         public void ListTransactionsTooHighLimit()
         {
-            var repo = new TransactionRepository();
+            var client = GetMockClient("");
+            var repo = new TransactionRepository(client.Object);
             repo.ListTransactions(201);
         }
     }
