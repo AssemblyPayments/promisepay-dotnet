371bfdfc5bdbc1d19d10617e43d5b2ae8208137d
Igor Sidorov <idsidorov@gmail.com>
2016-09-08 14:57:56 +0300

    Dynamic repos added in InitUnityContainer

--- PromisePayDotNet/DI/InitUnityContainer.cs
+++ PromisePayDotNet/DI/InitUnityContainer.cs
@@ -10,6 +10,8 @@ namespace PromisePayDotNet.DI
         public static void Init(IUnityContainer container)
         {
             container.RegisterType<IRestClient,RestClient>(new InjectionConstructor());
+
+            //Old style repositories
             container.RegisterType<IAddressRepository, AddressRepository>();
             container.RegisterType<IBankAccountRepository, BankAccountRepository>();
             container.RegisterType<ICardAccountRepository, CardAccountRepository>();
@@ -22,7 +24,27 @@ namespace PromisePayDotNet.DI
             container.RegisterType<IUploadRepository, UploadRepository>();
             container.RegisterType<IUserRepository, UserRepository>();
 
-
+            //Dynamic repositories
+            container.RegisterType<Dynamic.Interfaces.IAddressRepository, Dynamic.Implementations.AddressRepository>();
+            container.RegisterType<Dynamic.Interfaces.IBankAccountRepository, Dynamic.Implementations.BankAccountRepository>();
+            container.RegisterType<Dynamic.Interfaces.IBatchTransactionRepository, Dynamic.Implementations.BatchTransactionRepository>();
+            container.RegisterType<Dynamic.Interfaces.ICardAccountRepository, Dynamic.Implementations.CardAccountRepository>();
+            container.RegisterType<Dynamic.Interfaces.IChargeRepository, Dynamic.Implementations.ChargeRepository>();
+            container.RegisterType<Dynamic.Interfaces.ICompanyRepository, Dynamic.Implementations.CompanyRepository>();
+            container.RegisterType<Dynamic.Interfaces.IConfigurationRepository, Dynamic.Implementations.ConfigurationRepository>();
+            container.RegisterType<Dynamic.Interfaces.IDirectDebitAuthorityRepository, Dynamic.Implementations.DirectDebitAuthorityRepository>();
+            container.RegisterType<Dynamic.Interfaces.IFeeRepository, Dynamic.Implementations.FeeRepository>();
+            container.RegisterType<Dynamic.Interfaces.IItemRepository, Dynamic.Implementations.ItemRepository>();
+            container.RegisterType<Dynamic.Interfaces.IMarketplaceRepository, Dynamic.Implementations.MarketplaceRepository>();
+            container.RegisterType<Dynamic.Interfaces.IPayPalAccountRepository, Dynamic.Implementations.PayPalAccountRepository>();
+            container.RegisterType<Dynamic.Interfaces.IRestrictionRepository, Dynamic.Implementations.RestrictionRepository>();
+            container.RegisterType<Dynamic.Interfaces.ITokenRepository, Dynamic.Implementations.TokenRepository>();
+            container.RegisterType<Dynamic.Interfaces.IToolRepository, Dynamic.Implementations.ToolRepository>();
+            container.RegisterType<Dynamic.Interfaces.ITransactionRepository, Dynamic.Implementations.TransactionRepository>();
+            container.RegisterType<Dynamic.Interfaces.IUploadRepository, Dynamic.Implementations.UploadRepository>();
+            container.RegisterType<Dynamic.Interfaces.IUserRepository, Dynamic.Implementations.UserRepository>();
+            container.RegisterType<Dynamic.Interfaces.IWalletRepository, Dynamic.Implementations.WalletRepository>();
         }
+
     }
 }
